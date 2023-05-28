7eefb0c7c0d5f2aad0f55c161ed9a8c6e1edb6b6
Diệp Thu <118527110+Dieppthu@users.noreply.github.com>
2023-05-28 16:07:27 +0700

    Add files via upload

---
+++ BILL/ViewModel/Bill/BillVM.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BILL.ViewModel.Bill
+{
+    public class BillVM
+    {
+        public DateTime DateCreatBill { get; set; }
+        public DateTime DateOfPayment { get; set; }
+        public int Status { get; set; }
+        public Guid UserID { get; set; }
+    }
+}
