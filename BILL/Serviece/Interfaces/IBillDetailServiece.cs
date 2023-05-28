773dcb69fbf5b9a5599fe1ba6f3d67a0291c3535
Diệp Thu <118527110+Dieppthu@users.noreply.github.com>
2023-05-28 16:09:00 +0700

    Add files via upload

---
+++ BILL/Serviece/Interfaces/IBillDetailServiece.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ASMC5.Models;
+using BILL.ViewModel.Bill;
+
+namespace BILL.Serviece.Interfaces
+{
+    public interface IBillDetailServiece
+    {
+        public  Task<bool> CreatBillDetail(BillDetailVM p);
+        public Task<bool> EditBillDetail(Guid id, BillDetailVM p);
+        public Task<bool> DelBillDetail(Guid id);
+        public Task<List<BillDetail>> GetAllBillDetail();
+        public Task<BillDetail> GetBillDetailById(Guid id);
+    }
+}
