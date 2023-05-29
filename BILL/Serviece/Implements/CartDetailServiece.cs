8de3fec3b1e1021f368aea9cba6ca729caabdbe0
Diệp Thu <118527110+Dieppthu@users.noreply.github.com>
2023-05-29 14:41:39 +0700

    Add files via upload

---
+++ BILL/Serviece/Implements/CartDetailServiece.cs
@@ -0,0 +1,99 @@
+﻿using ASMC5.data;
+using ASMC5.Models;
+using BILL.Serviece.Interfaces;
+using BILL.ViewModel.Cart;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BILL.Serviece.Implements
+{
+    public class CartDetailServiece : ICartDetailServiece
+    {
+        ASMDBContext context;
+        public CartDetailServiece()
+        {
+            this.context = new ASMDBContext();
+        }
+        public async Task <bool> CreatCartDetail(CartDetailVM p)
+        {
+            try
+            {
+                var carts = await context.carts.FindAsync(p.CartID);
+                var product = await context.Products.FindAsync(p.ProductID);
+
+               
+                var Cartdetail = new CartDetail()
+                {
+                    ID = new Guid(),
+                    Quantity = p.Quantity,
+                    Status = 0,
+                    ProductID = product.ID,
+                    CartID = carts.UserId,
+                    Product = product,
+                    Cart = carts,
+                    
+                   
+                              
+                };
+                context.AddAsync(Cartdetail);
+                context.SaveChangesAsync();
+                return true;
+            }
+            catch (Exception)
+            {
+
+                return false;
+            }
+        }
+
+        public async Task< bool> DelCartDetail(Guid id)
+        {
+            try
+            {
+                var list = context.cartDetails.ToList();
+                var obj = list.FirstOrDefault(c => c.ID == id);
+                context.cartDetails.Remove(obj);
+                context.SaveChanges();
+                return true;
+            }
+            catch (Exception)
+            {
+
+                return false;
+            }
+        }
+
+        public async Task< bool> EditCartDetail(Guid id, CartDetailVM p)
+        {
+            try
+            {
+                var listobj = context.cartDetails.ToList();
+                var obj = listobj.FirstOrDefault(c => c.ID == id);
+                obj.Quantity = p.Quantity;               
+                context.cartDetails.Update(obj);
+                context.SaveChanges();
+                return true;
+
+            }
+            catch (Exception)
+            {
+
+                return false;
+            }
+        }
+
+        public async Task< List<CartDetail>> GetAllCartDetail()
+        {
+            return context.cartDetails.ToList();
+        }
+
+        public async Task< CartDetail> GetCartDetailById(Guid id)
+        {
+            var list = context.cartDetails.AsQueryable().ToList();
+            return list.FirstOrDefault(c => c.ID == id);
+        }
+    }
+}
---
+++ BILL/Serviece/Implements/CartServiece.cs
@@ -0,0 +1,92 @@
+﻿using ASMC5.data;
+using ASMC5.Models;
+using BILL.Serviece.Interfaces;
+using BILL.ViewModel.Cart;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BILL.Serviece.Implements
+{
+    public class CartServiece : ICartServiece
+    {
+        private readonly ASMDBContext _context;
+        public CartServiece()
+        {
+            _context = new ASMDBContext();
+        }
+        public async Task< bool> CreatCart(CartVN p)
+        {
+            try
+            {
+                var user = await _context.Users.FindAsync(p.UserId);
+                var cart = new Cart()
+                {
+                    UserId = user.Id,
+                    Description = p.Description,
+                    Status = p.Status,
+                    
+                };
+                await _context.AddAsync(cart);
+                await _context.SaveChangesAsync();
+                return true;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public async Task <bool> DelCart(Guid id)
+        {
+            try
+            {
+                var list = _context.carts.ToList();
+                var obj = list.FirstOrDefault(c => c.UserId == id);
+                _context.carts.Remove(obj);
+                _context.SaveChanges();
+                return true;
+            }
+            catch (Exception)
+            {
+
+                return false;
+            }
+        }
+
+        public async Task< bool> EditCart(Guid id, CartVN p)
+        {
+            try
+            {
+                var listobj = _context.carts.ToList();
+                var obj = listobj.FirstOrDefault(c => c.UserId == id);
+                obj.Description = p.Description;
+                obj.Status = p.Status;
+                _context.carts.Update(obj);
+                _context.SaveChanges();
+                return true;
+
+            }
+            catch (Exception)
+            {
+
+                return false;
+            }
+        }
+
+        public async Task< List<Cart>> GetAllCart()
+        {
+            return _context.carts.ToList();
+        }
+
+        public async Task <Cart> GetCartById(Guid id)
+        {
+            var list = _context.carts.AsQueryable().ToList();
+            return list.FirstOrDefault(c => c.UserId == id);
+        }
+    }
+}
