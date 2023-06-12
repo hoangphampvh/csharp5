59e946a242231a91557978a9aea48dc7dfc57e5e
Diệp Thu <118527110+Dieppthu@users.noreply.github.com>
2023-06-12 16:03:17 +0700

    Add files via upload

---
+++ ASMC5/Controllers/ProductController.cs
@@ -0,0 +1,88 @@
+﻿using BILL.ViewModel.Product;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Text;
+using ViewModel.ViewModel.Product;
+using ViewModel.ViewModel.Role;
+
+namespace ASMC5.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly HttpClient _httpClient;
+        public ProductController(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }       
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var response = await _httpClient.GetFromJsonAsync<List<ProductView>>($"https://localhost:7257/api/Product/GetAllProduct");
+            return View(response);
+
+        }
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create(ProductVM productcreat)
+        {
+            var productjson = JsonConvert.SerializeObject(productcreat);
+            HttpContent content = new StringContent(productjson, Encoding.UTF8, "application/json");
+            var response = await _httpClient.PostAsync("https://localhost:7257/api/Product/CreatProduct", content);
+            if (response.IsSuccessStatusCode)
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            else
+            {
+                return BadRequest();
+            }
+        }
+        [HttpGet]
+        public async Task<IActionResult> Details(Guid Id)
+        {
+            var response = await _httpClient.GetFromJsonAsync<ProductView>($"https://localhost:7257/api/Product/GetById/{Id}"); // sua ca thg nay nua nhe
+            return View(response);
+
+        }
+        [HttpGet]
+        public async Task<IActionResult> Edit(Guid Id)
+        {
+            var response = await _httpClient.GetFromJsonAsync<ProductVM>($"https://localhost:7257/api/Product/GetById/{Id}");
+            return View(response);
+
+        }
+        [HttpPost] // lưu ý là cái mvc này chỉ dùng đc http post và http get ; api thì dùng đc hết
+        public async Task<IActionResult> Edit(Guid Id, ProductVM proVM)
+        {
+            var Json = JsonConvert.SerializeObject(proVM);
+            HttpContent content = new StringContent(Json, Encoding.UTF8, "application/json");
+            var response = await _httpClient.PutAsync($"https://localhost:7257/api/Product/Update/{Id}", content);
+            if (response.IsSuccessStatusCode)
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            return View();
+
+        }
+
+
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(Guid Id)
+        {
+            var pro = await _httpClient.DeleteAsync($"https://localhost:7257/api/Product/Delete/{Id}");
+            if (pro.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return BadRequest("xoá không thành công");
+            }
+        }
+    }
+}
