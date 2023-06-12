545ad4099040290cf27c363f1d5645e459d145b1
Diệp Thu <118527110+Dieppthu@users.noreply.github.com>
2023-06-13 04:02:53 +0700

    Add files via upload

---
+++ ViewModel/ViewModel/Bill/BillDetailView.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ViewModel.ViewModel.Bill
+{
+    public class BillDetailView
+    {
+        public Guid ID { get; set; }
+        public string? CodeBill { get; set; }
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }      
+        public int Status { get; set; }
+        public Guid BillID { get; set; }
+        public Guid ProductID { get; set; }
+        public string NameProduct { get; set; }
+        public string UserName { get; set; } 
+    }
+}
