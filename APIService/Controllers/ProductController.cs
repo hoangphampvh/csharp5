230fd4c51116bc830e38adbe7da8ee9c2d69373b
Diệp Thu <118527110+Dieppthu@users.noreply.github.com>
2023-06-12 16:01:06 +0700

    Add files via upload

---
+++ APIService/Controllers/ProductController.cs
@@ -0,0 +1,57 @@
+﻿using BILL.ViewModel.Product;
+using BILL.Serviece.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+
+namespace APIServer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly IProductServiece _productServiece;
+        
+        public ProductController(IProductServiece productServiece)
+        {
+            _productServiece = productServiece;
+        }
+
+        [HttpGet("GetAllProduct")]
+        public async Task<IActionResult> GetAllAsync()
+        {
+            var products = await _productServiece.GetAllProduct();
+            return Ok(products);
+            
+        }
+
+        [HttpPost("CreatProduct")]
+
+        public async Task<IActionResult> CreatProductAsync([FromBody]ProductVM product)
+        {
+            var pro = await _productServiece.CreatProduct(product);
+            return Ok(pro);
+        }
+        [HttpDelete("Delete/{Id}")]
+
+        public async Task<IActionResult> DeleteAsync(Guid Id)
+        {
+            var result = await _productServiece.DelProduct(Id);
+            return Ok(result);
+        }
+        [HttpGet("GetById/{id}")]
+        public async Task<IActionResult> GetById(Guid id)
+        {
+            var pro = await _productServiece.GetProductById(id);
+            if (pro != null) return Ok(pro);
+            return BadRequest();
+        }
+        [HttpPut("Update/{id}")]
+
+        public async Task<IActionResult> UpdateAsync(Guid id,[FromBody]ProductVM product)
+        {
+            var result = await _productServiece.EditProduct(id,product);
+            return Ok(result);
+        }
+    }
+}
